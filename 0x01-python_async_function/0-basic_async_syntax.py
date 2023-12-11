c40675bc1980be947802fb8bd0a9f8dfcc3be840
nandz080 <nandz080@gmail.com>
2023-12-11 23:17:03 +0000

    [task 0] Write an asynchronous coroutine that takes in an integer arg that waits for random delay

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Write an asynchronous coroutine that takes in an integer
+    argument named wait_random that waits for a random delay 
+    between 0 and max_delay seconds and eventually returns
+    it
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """ Class that waits for a random number of seconds
+    """
+    wait_time: float = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
