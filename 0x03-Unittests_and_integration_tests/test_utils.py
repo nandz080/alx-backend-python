7704771020138047e4fb19d2ae874d106920dab9
nandz080 <nandz080@gmail.com>
2024-05-17 16:10:26 +0000

    [task 1] Implement TestAccessNestedMap.test_access_nested_map_exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,8 +19,8 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
-        ({}, ("a",), "a"),
-        ({"a": 1}, ("a", "b"), "b")
+        ({}, ("a",), "'a'"),
+        ({"a": 1}, ("a", "b"), "'b'")
     ])
     def test_access_nested_map_exception(self, nested_map, path, expected_exception_msg):
         """Test that access_nested_map raises KeyError for invalid paths."""
